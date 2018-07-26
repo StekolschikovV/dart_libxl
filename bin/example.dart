af1000707c27b7d773f051f4c1b7dcc886494653
stekolschikov <stekolschikov@mail.ua>
2018-07-26 20:37:21 +0500

    XlSheet add write

--- bin/example.dart
+++ bin/example.dart
@@ -26,6 +26,7 @@ main() {
 
   var format = book.addFormat();
   format.formatSetBorderBottom(3);
+  format.formatSetRotation(1);
   sheet.writeNum(1,0,1.1,format);
   sheet.writeStr(3,0,'Hello',format);
 
--- lib/src/sheet.dart
+++ lib/src/sheet.dart
@@ -15,17 +15,68 @@ class XlSheet {
     return 'XlSheet{ptr: $ptr}';
   }
 
-  writeStr(int row, int col, String value, [var f = 0]) {
-    var res = _xlSheetWriteStr(ptr, row, col, value, f == 0 ? 0 : f.ptr);
+  writeStr(int row, int col, String value, [var format = 0]) {
+    var res = _xlSheetWriteStr(ptr, row, col, value, format == 0 ? 0 : format.ptr);
     if (res == 0) {
       throw XlException(book.errorMessage());
     }
   }
 
-  writeNum(int row, int col, double value, [var f = 0]) {
-    var res = _xlSheetWriteNum(ptr, row, col, value, f == 0 ? 0 : f.ptr);
+  writeNum(int row, int col, double value, [var format = 0]) {
+    var res = _xlSheetWriteNum(ptr, row, col, value, format == 0 ? 0 : format.ptr);
     if (res == 0) {
       throw XlException(book.errorMessage());
     }
   }
+
+  writeBool(int row, int col, int value, [var format = 0]) {
+    var res = _xlSheetWriteBool(ptr, row, col, value, format == 0 ? 0 : format.ptr);
+    if (res == 0) {
+      throw XlException(book.errorMessage());
+    }
+  }
+
+  writeBlank(int row, int col, [var format = 0]) {
+    var res = _xlSheetWriteBlank(ptr, row, col, format == 0 ? 0 : format.ptr);
+    if (res == 0) {
+      throw XlException(book.errorMessage());
+    }
+  }
+
+  writeFormula(int row, int col, String value, [var format = 0]) {
+    var res = _xlSheetWriteFormula(ptr, row, col, value, format == 0 ? 0 : format.ptr);
+    if (res == 0) {
+      throw XlException(book.errorMessage());
+    }
+  }
+
+  writeFormulaNum(int row, int col, String expr, num value, [var format = 0]) {
+    var res = _xlSheetWriteFormulaNum(ptr, row, col, expr, value, format == 0 ? 0 : format.ptr);
+    if (res == 0) {
+      throw XlException(book.errorMessage());
+    }
+  }
+
+  writeFormulaStr(int row, int col, String expr, String value, [var format = 0]) {
+    var res = _xlSheetWriteFormulaStr(ptr, row, col, expr, value, format == 0 ? 0 : format.ptr);
+    if (res == 0) {
+      throw XlException(book.errorMessage());
+    }
+  }
+
+  writeFormulaBool(int row, int col, String expr, int value, [var format = 0]) {
+    var res = _xlSheetWriteFormulaBool(ptr, row, col, expr, value, format == 0 ? 0 : format.ptr);
+    if (res == 0) {
+      throw XlException(book.errorMessage());
+    }
+  }
+
+  writeComment(int row, int col, String value, String author, int width, int height) {
+    _xlSheetWriteComment(ptr, row, col, value, author, width, height);
+  }
+
+  writeError(int row, int col, int error, [var format = 0]) {
+    _xlSheetWriteError(ptr, row, col, error, format == 0 ? 0 : format.ptr);
+  }
+
 }
