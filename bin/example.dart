2fa4923ef0d1ef890b6af7d639acc82943ce9c8d
stekolschikov <stekolschikov@mail.ua>
2018-07-28 12:48:44 +0500

    XlBook add all methods

--- bin/example.dart
+++ bin/example.dart
@@ -34,14 +34,6 @@ main() {
   formatCopy.setAlignV(AlignH.ALIGNH_CENTER);
   sheet.writeNum(1,0,555.0, formatCopy);
 
-
-//  var format = book.addFormat();
-//  format.formatSetBorderBottom(3);
-//  format.formatSetRotation(1);
-//  sheet.writeNum(1,0,1.1,format);
-//  sheet.writeStr(3,0,'Hello',format);
-
-
   book.addSheet('CopySheet', sheet);
 
   var fileName = 'test.xls';
